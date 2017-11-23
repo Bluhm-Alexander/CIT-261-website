6c12166df6272445a5ec326bb10618b1de9dd988
Bluhm-Alexander <alexbluhm1993@gmail.com>
2017-11-23 10:56:16 -0700

    more pages

---
+++ Week08/Week08-Coding.js
@@ -0,0 +1,19 @@
+/*
+*	Week08-Coding.js
+*   Author: Alexander Bluhm
+*/
+//Using local storage API to store information
+
+function problem1() {
+	
+	//First we are going to do a check to see if local storage is 
+	//available to us. If not display alert.
+	if (typeof(Storage) !== "undefined") {
+		
+	}
+	else {
+		alert("warning cannot open Local Storage");
+	}
+
+
+}
