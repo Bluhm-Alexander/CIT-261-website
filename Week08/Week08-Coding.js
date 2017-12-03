4e0d7c7efdae396ec991ae5ae9b93fd3b97ba36e
Bluhm-Alexander <31932943+Bluhm-Alexander@users.noreply.github.com>
2017-12-02 23:45:51 -0700

    added week08 stuff

--- Week08/Week08-Coding.js
+++ Week08/Week08-Coding.js
@@ -5,15 +5,25 @@
 //Using local storage API to store information
 
 function problem1() {
+	var object = [];
 	
+	object.firstName = document.getElementById("firstname").value;
+	object.lastName = document.getElementById("lastname").value;
+	object.gender = document.getElementById("gender").value;
+	
+	document.getElementById("demo").innerHTML = object.firstName;
 	//First we are going to do a check to see if local storage is 
 	//available to us. If not display alert.
+	sessionStorage.stored = "john";
 	if (typeof(Storage) !== "undefined") {
-		
+		sessionStorage.setItem('object', JSON.stringify(object));
 	}
+	
 	else {
 		alert("warning cannot open Local Storage");
 	}
 
-
+	//retrieve object from lesson storage import in new
+	//var newObject = JSON.parse(sessionStorage.stored);
+	document.getElementById("demo").innerHTML = sessionStorage.stored;
 }
