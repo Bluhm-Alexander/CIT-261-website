91ccb5e1549354c30400511e11e1574989a4c4ec
Alex Bluhm <31932943+Bluhm-Alexander@users.noreply.github.com>
2017-12-05 16:24:01 -0700

    Fixed Index

--- Week11/Week11-Coding.js
+++ Week11/Week11-Coding.js
@@ -9,6 +9,7 @@
 function start() {
 	problem1();
 	problem2();
+	controls();
 }
 
 function problem1() {
@@ -41,6 +42,53 @@ function changeWhite() {
 	var elem = document.getElementById('touch');
 	elem.style.transition = "background 1.0s linear 0s";
 	elem.style.background = "#ffffff";
+}
+
+/*********************************************************************************
+* This Next Section is for My animated mario Sprite
+*
+*********************************************************************************/
+function leftArrowPressed() {
+	var element = document.getElementById("image1");
+	element.style.left = parseInt(element.style.left) - 5 + 'px';
+}
+
+function rightArrowPressed() {
+	var element = document.getElementById("image1");
+	element.style.left = parseInt(element.style.left) + 5 + 'px';
+}
+
+				function upArrowPressed() {
+				var element = document.getElementById("image1");
+				element.style.top = parseInt(element.style.top) - 5 + 'px';
+				}
+
+				function downArrowPressed() {
+				var element = document.getElementById("image1");
+				element.style.top = parseInt(element.style.top) + 5 + 'px';
+				}
+
+				function moveSelection(evt) {
+					switch (evt.keyCode) {
+						case 37:
+						leftArrowPressed();
+						break;
+						case 39:
+						rightArrowPressed();
+						break;
+						case 38:
+						upArrowPressed();
+						break;
+						case 40:
+						downArrowPressed();
+						break;
+						}
+					};
+
+function controls()
+{
+	//Event Li
+	window.addEventListener('keydown', moveSelection);
 }
 
 window.onload = start;
